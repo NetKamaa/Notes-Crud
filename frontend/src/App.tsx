48ebde229e3c2cea0980437dc8b9e85b44228624
Tenderly__ <olevnao31@gmail.com>
2026-05-03 17:09:54 +0300

    chore: initialize project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="min-h-screen bg-slate-950 text-white p-8">
+        <h1 className="text-3xl font-bold">Notes CRUD</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
